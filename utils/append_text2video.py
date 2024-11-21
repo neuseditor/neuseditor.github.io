1bc3d7366700a4f00c4ab80fe1fd69e79df36ccb
neuseditor <mirnail994+neuseditor@gmail.com>
2024-11-21 21:40:58 +0100

    upload background editting

--- utils/append_text2video.py
+++ utils/append_text2video.py
@@ -9,30 +9,34 @@ video_path = "4_bears_fg.mp4"  # Replace with your video file path
 output_video_path = "4_bears_fg_text.mp4"  # Replace with your output video file path
 video_path = "optimization_track.mp4"  # Replace with your video file path
 output_video_path = "optimization_track_text.mp4"  # Replace with your output video file path
+video_path = "../static/videos/bear_edit_bg.mp4"  # Replace with your video file path
+output_video_path = "../static/videos/bear_edit_bg_text.mp4"  # Replace with your output video file path
+
 video = VideoFileClip(video_path)
 
 # Create text clips
 # texts = ["Unseen views", "Background", "Foreground", "Geometry (Normal map)"]
 # texts = ["Unseen [Identity] views", "Make it a grizzly bear", "Make it a panda", "Make it a polar bear"]
-texts = ["GT view", "Learned view","Editted scene","Background","Foreground", "Depth map","Normal map"]
 
 text_clips = []
 
 # Set font size and colors
-fontsize = 30
+fontsize = 72
 text_color = 'white'
 bg_color = 'black'
 padding_height = 100  # Height of the text box
 
 # Create a text clip for each text
+texts = ["Identity", "add cactuses", "make it winter", "in a cave"]
 for text in texts:
-    text_clip = TextClip(text, fontsize=fontsize, color=text_color, bg_color=bg_color, size=(video.w / len(texts), padding_height))
+    font = 'Arial' if text == "Identity" else 'Apple-Chancery'
+    text_clip = TextClip(text, fontsize=fontsize, font=font, color=text_color, bg_color=bg_color, size=(video.w / len(texts), padding_height))
     text_clips.append(text_clip.set_duration(video.duration))
 
 # Positioning the text clips
 # Starting from the center and adjusting horizontal offsets
-offsets = [0, 994, 1988, 2982]  # Adjust these values to disturb horizontally
-offsets = [0, 320, 640, 960, 1280, 1600, 1920]  # Adjust these values to disturb horizontally
+offsets = [0, 1000, 2000, 3000]  # Adjust these values to disturb horizontally
+# offsets = [0, 320, 640, 960, 1280, 1600, 1920]  # Adjust these values to disturb horizontally
 
 text_positions = [(offset, video.h - padding_height) for offset in offsets]
 
